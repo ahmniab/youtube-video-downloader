5f0447ece625dfd1e2820d35cd2d4e84e64995d0
ahmniab <ahmniab11@gmail.com>
2025-01-26 19:01:21 +0200

    open browser tap

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from http.server import HTTPServer
 from request_handler import HTTPRequestHandler
 import helper
+import webbrowser
+
 # Run the server
 if __name__ == "__main__":
 
     settings = helper.get_settings()
     server_address = ("", settings["port"])
     httpd = HTTPServer(server_address, HTTPRequestHandler)
-    print(f"Server running on http://localhost:{settings["port"]}")
+    server_url = f'http://localhost:{settings["port"]}'
+    print(f"Server running on {server_url}")
+    webbrowser.open_new_tab(server_url)
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
